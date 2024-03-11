a3c4b014578756887c5bd7729168abfba123673e
concerned-doggo <codeformetoo@proton.me>
2024-03-11 22:27:11 +0530

    add alternate color change rofi-menu

--- .config/qtile/scripts/allowScripts.sh
+++ .config/qtile/scripts/allowScripts.sh
@@ -6,3 +6,7 @@ chmod u+x  ~/.config/qtile/scripts/volume/mute.sh
 
 # rofi scripts
 chmod u+x ~/.config/rofi/scripts/power
+
+# install scripts
+chmod u+x ~/.config/qtile/scripts/install.sh
+chmod u+x ~/.config/qtile/scripts/installYay.sh
