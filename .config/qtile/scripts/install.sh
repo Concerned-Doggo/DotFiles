27a93e0877fc6f3fab2007a5dc9dd47ec9bcff23
concerned-doggo <codeformetoo@proton.me>
2024-07-02 05:40:13 +0000

    add fonts and pipewire audio installation

--- .config/qtile/scripts/install.sh
+++ .config/qtile/scripts/install.sh
@@ -1,1 +1,1 @@
-sudo pacman -Sy git neovim dunst picom rofi rofi-emoji rofi-calc pamixer stow
+sudo pacman -Sy git neovim dunst picom rofi rofi-emoji rofi-calc pamixer stow pipewire pipewire-pulse wireplumber pipewire-alsa
